6d958dee91e03ad52ae2085c6b2722f226bd2130
Max Velikanov <maximelianos.m@gmail.com>
2022-07-17 15:13:49 +0300

    '1'

--- train_vgg.py
+++ train_vgg.py
@@ -19,6 +19,26 @@ import matplotlib.pyplot as plt
 
 DEVICE = 'cuda'
 
+# read CSV (duplicate from repo)
+IMG_KEY = 'img_basename'
+DAT_KEY = 'dataset_name'
+
+def check(table):
+    per_img_count = table[[IMG_KEY, DAT_KEY]].groupby(IMG_KEY).count()
+    dataset_count = len(table[DAT_KEY].unique())
+    if not (per_img_count.values == dataset_count).all():
+        problem_images = per_img_count[per_img_count.values != dataset_count].index.values
+        print("Warning: in pd.DataFrame index is not complete!")
+        print(f"Following {len(problem_images)} image(s) do not exist in every dataset")
+        print(problem_images)
+
+def read_results_csv(csv_fname):
+    dataframe = pd.read_csv(csv_fname, dtype={IMG_KEY: str, DAT_KEY: str})
+    dataframe = dataframe.sort_values([IMG_KEY, DAT_KEY])
+    check(dataframe)
+    return dataframe
+
+
 
 class cocoDataset(data.Dataset):
     """
@@ -33,18 +53,24 @@ class cocoDataset(data.Dataset):
         pattern = os.path.join(dataset_root, "coco_5k_v3_decoded/*/*")
         self.image_path = sorted(glob(pattern + ".png") + glob(pattern + ".jpg"))
 
+        self.df = read_results_csv(os.path.join(dataset_root, "coco_5k_results/merged.csv"))
+
     def __getitem__(self, index):
         index = index % len(self.image_path)
         image = cv2.imread(self.image_path[index])[:, :, ::-1].astype(np.float32) / 255
 
         path = Path(self.image_path[index])
         name = path.stem
-        degrad = path.parent.name
+        codec = path.parent.name
 
         input_size = 224
         image = cv2.resize(image, (input_size, input_size))
 
-        return name, degrad, torch.from_numpy(image), torch.tensor( [np.mean(image)] )
+        # get mean-iou for image from dataframe
+        gt = self.df[(self.df["img_basename"] == name) & (self.df["dataset_name"] == codec)]["yolov5s"]
+        gt = gt[0]
+
+        return name, codec, torch.from_numpy(image), torch.tensor( [gt] )
 
     def __len__(self):
         return len(self.image_path)
