d6350909f2691bafe7bd0b63a9631a5974bd6277
Max Velikanov <maximelianos.m@gmail.com>
2022-08-02 10:04:12 +0300

    1

--- polus_scripts/keep_alive.sh
+++ polus_scripts/keep_alive.sh
@@ -1,6 +1,7 @@
 rel_path=$(dirname "$0")                # relative
 abs_path=$(cd "$rel_path" && pwd)       # absolutized and normalized
 
+cd "$rel_path"
 while true; do
     if test -f "run.lock"; then         # file exists, wait
         sleep 1
